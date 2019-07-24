db08ef1ec047fdf874aa4719c60efd95754398d6
liuhuihao <1061036802@qq.com>
2019-07-24 14:17:35 +0800

    feat: 基本样式添加

--- src/main/index.js
+++ src/main/index.js
@@ -1,28 +1,19 @@
-'use strict'
-
-import {app, BrowserWindow} from 'electron'
-
-/**
- * Set `__static` path to static files in production
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
- */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
-}
+import {app, BrowserWindow, Menu} from 'electron'
 
 let mainWindow
-const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
+const winURL = process.env.NODE_ENV === 'development' ? `http://localhost:9080` : `file://${__dirname}/index.html`
 
-function createWindow () {
-  /**
-   * Initial window options
-   */
+// 创建主窗体
+const createWindow = () => {
+  Menu.setApplicationMenu(null)
   mainWindow = new BrowserWindow({
-    height: 563,
-    useContentSize: true,
-    width: 1000
+    width: 1008,
+    height: 950,
+    minWidth: 1008,
+    minHeight: 950,
+    maxWidth: 1008,
+    maxHeight: 950,
+    useContentSize: true
   })
 
   mainWindow.loadURL(winURL)
@@ -32,7 +23,9 @@ function createWindow () {
   })
 }
 
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow()
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -45,23 +38,3 @@ app.on('activate', () => {
     createWindow()
   }
 })
-
-/**
- * Auto Updater
- *
- * Uncomment the following code below and install `electron-updater` to
- * support auto updating. Code Signing with a valid certificate is required.
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
- */
-
-/*
-import { autoUpdater } from 'electron-updater'
-
-autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
-})
-
-app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
-})
- */
--- src/renderer/pages/home/index.vue
+++ src/renderer/pages/home/index.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="home-page">
 
-    <el-input placeholder="请输入页面地址" v-model="url">
+    <!-- 顶部地址输入框 -->
+    <el-input placeholder="请输入页面地址" v-model="url" class="url-input">
       <template slot="prepend">页面地址：</template>
+      <template slot="append">→</template>
     </el-input>
 
-    <div class="webview-container">
-      <div class="webview-header"></div>
-      <webview class="webview" :src="url"></webview>
+
+    <div class="page-container">
+
+      <!-- Webview -->
+      <div class="webview-container">
+        <div class="webview-header">
+          <div class="back"><</div>
+          <h1>TITLE</h1>
+          <div class="forward">></div>
+        </div>
+        <webview class="webview" :src="url"></webview>
+      </div>
+
+      <!-- 中部按钮组 -->
+      <div class="btn-container">
+        <i class="fa fa-mouse-pointer" :class="{active:selecting}" @click="toggleSelect"></i>
+      </div>
+
+      <!-- 右侧列表 -->
+      <div class="event-container">
+        1234 TODO
+      </div>
     </div>
 
-    <button @click="startSelect">start select</button>
-    <button @click="stopSelect">end select</button>
   </div>
 </template>
 
@@ -23,7 +42,8 @@
     data () {
       return {
         webview: null, // webview 对象
-        url: 'https://www.github.com/' // 页面地址
+        url: 'https://www.github.com/', // 页面地址
+        selecting: false // 是否正在选取元素
       }
     },
     methods: {
@@ -40,15 +60,18 @@
         })
       },
 
-      startSelect () {
-        const insert = 'selectPlugin.startSelect()'
-        this.webview.executeJavaScript(insert)
-      },
-
-      stopSelect () {
-        const insert = 'selectPlugin.stopSelect()'
+      // 切换 Dom 选择
+      toggleSelect () {
+        let insert = ''
+        if (this.selecting) {
+          insert = 'selectPlugin.stopSelect()'
+        } else {
+          insert = 'selectPlugin.startSelect()'
+        }
+        this.selecting = !this.selecting
         this.webview.executeJavaScript(insert)
       }
+
     },
     mounted () {
       this.init()
